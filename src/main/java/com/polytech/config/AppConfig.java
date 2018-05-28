d8f69d38e2b4f0165d9a759ffe25676aec4b6606
DamienGaudard <damien.gaudard06@gmail.com>
2018-05-28 12:01:01 +0200

    Mini projet polykeep

--- src/main/java/com/polytech/config/AppConfig.java
+++ src/main/java/com/polytech/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.polytech.persistence.JdbcUserRepository;
 import com.polytech.persistence.UserRepo;
 import com.polytech.service.UserService;
 import com.polytech.web.RegisterController;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,7 +13,6 @@ import com.polytech.persistence.StoryRepository;
 import com.polytech.service.FeedService;
 import com.polytech.service.PublicationService;
 import com.polytech.web.FeedController;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -54,9 +54,9 @@ public class AppConfig {
 
     @Bean
     public DataSource dataSource(){
-        return new EmbeddedDatabaseBuilder()
-                .build();
-       /* String url = "jdbc:mysql://localhost:3306/polytech";
+        /*return new EmbeddedDatabaseBuilder()
+                .build();*/
+        String url = "jdbc:mysql://localhost:3306/polytech";
         String username="root";
         String password="root";
 
@@ -64,7 +64,7 @@ public class AppConfig {
         dataSource.setJdbcUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-        return dataSource;*/
+        return dataSource;
     }
     @Bean
     public UserRepo userRepo (DataSource dataSource){
